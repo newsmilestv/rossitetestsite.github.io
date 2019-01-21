38ac85cfc92e1ce92e152d16c320370cd16085c0
Leonid Second Comp <li_che_in@mail.ru>
2019-01-21 13:28:15 +0400

    End commit

--- js/myscript.js
+++ js/myscript.js
@@ -2,9 +2,14 @@
   var button = document.getElementById('start-demo'),
       background = document.getElementById('wrapp');
 
-      setInterval(function () {
-        button.onclick = background.style.visibility = 'hidden';
-      } , 5000);
+      // setTimeout(function () {
+      //   button.onclick = background.style.visibility = 'hidden';
+      // } , 5000);
 
-
+      button.onclick =function () {
+        setTimeout(function () {
+          // background.style.visibility = 'hidden';
+          alert('HI!!!');
+        } , 5000);
+      };
 })();
